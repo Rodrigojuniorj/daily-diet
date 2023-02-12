47209a2894f4370406ce5f5d7b16816770131b5e
Rodrigo Junior <rodrigo.franco@unifio.edu.br>
2023-02-12 19:53:02 -0300

    feat:start project init

---
+++ src/theme/index.ts
@@ -0,0 +1,31 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+
+    RED: '#F75A68',
+    RED_DARK: '#AA2834',
+
+    GRAY_1: '#1B1D1E',
+    GRAY_2: '#333638',
+    GRAY_3: '#5C6265',
+    GRAY_4: '#B9BBBC',
+    GRAY_5: '#DDDEDF',
+    GRAY_6: '#DDDEDF',
+    GRAY_7: '#FAFAFA',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    BOLD: 'Nunito_700Bold'
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    '2XL': 32
+  }
+};
